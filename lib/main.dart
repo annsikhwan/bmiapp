f89e33ce7b4ac162ca975c29189fa7bdb84486cc
annsikhwan <75122613+annsikhwan@users.noreply.github.com>
2023-04-19 12:37:08 +0500

    Update main.dart
    part 2 done

--- lib/main.dart
+++ lib/main.dart
@@ -37,4 +37,29 @@ class _InputPageState extends State<InputPage> {
           color: Color(0xFF1D1E33),
           borderRadius: BorderRadius.circular(10.0)),
     ),
+    ),Expanded(
+    child: null,
+    ),
+    ],
+    ),
+    ),
+    Expanded(
+    child: Container(
+    margin: EdgeInsets.all(15.0),
+    decoration: BoxDecoration(
+    color: Color(0xFF1D1E33),
+    borderRadius: BorderRadius.circular(10.0)),
+    ),
+    ),
+    Expanded(
+    child: Expanded(
+    child: Row(
+    children: <Widget>[
+    Expanded(
+    child: Container(
+    margin: EdgeInsets.all(15.0),
+    decoration: BoxDecoration(
+    color: Color(0xFF1D1E33),
+    borderRadius: BorderRadius.circular(10.0)),
+    ),
     ),
