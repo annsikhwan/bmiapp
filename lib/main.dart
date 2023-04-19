7485f67cd2c9c70f7764f30015883a2d56bb1a3d
annsikhwan <75122613+annsikhwan@users.noreply.github.com>
2023-04-19 12:50:27 +0500

    Update main.dart
    last part

--- lib/main.dart
+++ lib/main.dart
@@ -63,3 +63,37 @@ class _InputPageState extends State<InputPage> {
     borderRadius: BorderRadius.circular(10.0)),
     ),
     ),
+      Expanded(
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0)),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+    ),
+    ),
+    ),
+          ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
